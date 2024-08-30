c8eda27303f4270c6a1d0dbc7c0c767aac0a4c32
biapagotto <biapagotto.ux@gmail.com>
2024-08-30 12:36:43 -0300

    Atualização 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Provider } from 'react-redux'
 import { store } from './store/store'
 
-// eslint-disable-next-line react/no-deprecated
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
